3cfbdd0f2bc93050f2c67f7f293e7a7ab8bd19f5
Devin Powers <powers88@msu.edu>
2020-12-02 09:00:48 -0500

    update

--- Text_Editor.py
+++ Text_Editor.py
@@ -1,2 +1,66 @@
 '''Text Editor'''
 
+from tkinter import *
+
+from tkinter import filedialog
+
+from tkinter import messagebox
+
+from tkinter import colorchooser
+
+
+
+import datetime
+
+
+
+
+def line():
+
+    lin = "_" * 60
+
+    text.insert(INSERT,lin)
+
+
+def date():
+
+    data = datetime.date.today()
+
+    text.insert(INSERT,data)
+
+   
+
+def normal():
+
+    text.config(font = ("Arial", 12))
+
+
+
+def bold():
+
+    text.config(font = ("Arial", 12, "bold"))
+
+
+
+def underline():
+
+    text.config(font = ("Arial", 12, "underline"))
+
+
+
+def italic():
+
+    text.config(font = ("Arial",12,"italic"))
+
+    
+
+def font():
+
+    (triple,color) = askcolor()
+
+    if color:
+
+       text.config(foreground=color)
+
+
+
